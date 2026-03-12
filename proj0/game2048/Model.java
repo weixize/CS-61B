5e4d7ba70c6421910b31f30958926ba7bfe9cede
Xize <weixize375@gmail.com>
2026-03-12 10:41:43 +0800

    passing all tests for maxTileExists method

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -155,6 +155,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (int col = 0; col < b.size(); col = col + 1) {
+            for (int row = 0; row < b.size(); row = row + 1) {
+                if (b.tile(col, row) != null && b.tile(col, row).value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
