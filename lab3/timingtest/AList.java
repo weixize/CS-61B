7ace40fcb4cf91c62c95eef23892bbfeb7a03791
Xize <weixize375@gmail.com>
2026-05-01 01:27:03 +0800

    fix 2) TestTimeAListTableValues (0/14.545)

--- lab3/timingtest/AList.java
+++ lab3/timingtest/AList.java
@@ -34,7 +34,7 @@ public class AList<Item> {
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
         if (size == items.length) {
-            resize((int) Math.round(size * 1.01));
+            resize((int) Math.round(size * 2));
         }
 
         items[size] = x;
