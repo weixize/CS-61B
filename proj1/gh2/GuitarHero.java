d15994daec72aa8467c64a0fd9e447bccd7ec070
Xize <weixize375@gmail.com>
2026-07-24 16:44:47 +0800

    done with GuiterHero.java

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,46 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
+public class GuitarHero {
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+
+    public static void main(String[] args) {
+        /* create two guitar strings, for concert A and C */
+        GuitarString[] concerts = new GuitarString[37];
+        for (int i = 0; i < 37; i += 1) {
+            concerts[i] = new GuitarString(440 * Math.pow(2, (double) (i - 24) / 12));
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index >= 0) {
+                    concerts[index].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (GuitarString gs : concerts) {
+                sample += gs.sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (GuitarString gs : concerts) {
+                gs.tic();
+            }
+        }
+    }
+}
+
