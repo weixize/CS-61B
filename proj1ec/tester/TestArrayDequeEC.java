e0985f818ad32a3385e69869aaa466ca012e0314
Xize <weixize375@gmail.com>
2026-07-26 16:04:38 +0800

    done with proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -15,30 +15,58 @@ public class TestArrayDequeEC {
             ArrayDequeSolution<Integer> aas = new ArrayDequeSolution<>();
 
             int N = 5000;
+            int[][] cache = new int[N][];
             for (int i = 0; i < N; i += 1) {
                 int operationNumber = StdRandom.uniform(0, 4);
                 if (operationNumber == 0) {
                     int randVal = StdRandom.uniform(0, 100);
                     sad.addLast(randVal);
                     aas.addLast(randVal);
+                    cache[i] = new int[]{operationNumber, randVal};
                 } else if (operationNumber == 1) {
                     if (!aas.isEmpty()) {
                         Integer lastSad = sad.removeLast();
                         Integer lastAas = aas.removeLast();
-                        assertEquals(lastSad, lastAas);
+                        cache[i] = new int[]{operationNumber};
+                        assertEquals(construct(cache), lastSad, lastAas);
                     }
                 } else if (operationNumber == 2) {
                     int randVal = StdRandom.uniform(0, 100);
                     sad.addFirst(randVal);
                     aas.addFirst(randVal);
+                    cache[i] = new int[]{operationNumber, randVal};
                 } else if (operationNumber == 3) {
                     if (!aas.isEmpty()) {
                         Integer firstSad = sad.removeFirst();
                         Integer firstAas = aas.removeFirst();
-                        assertEquals(firstSad, firstAas);
+                        cache[i] = new int[]{operationNumber};
+                        assertEquals(construct(cache), firstSad, firstAas);
                     }
                 }
             }
         }
+    }
+
+    private static String construct(int[][] cache) {
+        StringBuilder returnSB = new StringBuilder();
+        for (int[] ints : cache) {
+            if (ints == null) {
+                continue;
+            }
+            if (ints[0] == 0) {
+                returnSB.append("addFirst(");
+                returnSB.append(ints[1]);
+                returnSB.append(")\n");
+            } else if (ints[0] == 1) {
+                returnSB.append("removeLast()\n");
+            } else if (ints[0] == 2) {
+                returnSB.append("addFirst(");
+                returnSB.append(ints[1]);
+                returnSB.append(")\n");
+            } else if (ints[0] == 3) {
+                returnSB.append("removeFirst()\n");
+            }
+        }
+        return returnSB.toString();
     }
 }
